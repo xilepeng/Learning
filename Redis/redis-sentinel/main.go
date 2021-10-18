e557107f2877f889d5b0734fcbbd822c3daae650
xilepeng <lepengxi@gmail.com>
2021-10-18 12:01:49 +0800

    redis

---
+++ Redis/redis-sentinel/main.go
@@ -0,0 +1,23 @@
+package main
+ 
+import (
+        "fmt"
+        "github.com/go-redis/redis"
+        "time"
+)
+ 
+func main() {
+        client := redis.NewFailoverClient(&redis.FailoverOptions{
+                MasterName:    "mymaster",
+                SentinelAddrs: []string{"127.0.0.1:26379", "127.0.0.1:26380", "127.0.0.1:26381"},
+                Password:      "",
+                DB:            0,
+        })
+ 
+        for {
+                reply, err := client.Incr("pvcount").Result()
+                fmt.Printf("reply=%v err=%v\n", reply, err)
+                time.Sleep(1 * time.Second)
+        }
+}
+
